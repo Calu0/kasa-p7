c77b8a12ae95544354d0f5b0e3ae4a71b101e02f
Luca <luca.kanto@gmail.com>
2023-06-17 13:00:53 +0200

    slideshow done

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -8,18 +8,72 @@ import styled from "styled-components";
 
 
 
-function Slideshow(props){
+function Slideshow(props) {
 
     const { pictures } = props;
     const [currentIndex, setCurrentIndex] = useState(0)
 
-    return(
-        <div className='slideshow-container'>
-            {pictures.map((picture, index) => 
-                <img src={picture} alt='picture' className='picture' key={`${picture}-${index}`}></img>
-            )}
+    function hide() {
+        if (pictures.length === 1) {
+          return "none";
+        }
+      }
 
-        </div>       
+    function indexPlus(){
+        if(currentIndex + 1< pictures.length) {
+            setCurrentIndex(currentIndex + 1)
+        }
+        else{
+            setCurrentIndex(0)
+        }
+    }
+
+  
+    function indexMinus(){
+        if(currentIndex + 1 > 1) {
+            setCurrentIndex(currentIndex - 1)
+        }
+        else{
+            setCurrentIndex(pictures.length - 1)
+        }
+    }
+
+    //const indexMinusT =  currentIndex + 1 > 1 ? setCurrentIndex(currentIndex - 1) : setCurrentIndex(pictures.length - 1);
+
+    return (
+        <div className='slideshow-container'>
+    
+                <div className='slideshow-wrapper'>
+
+                    <img src={pictures[currentIndex]} alt='picture' className='picture'></img>
+
+                    <div className='dir-arrow-container'>
+                        <img 
+
+                        src={arrow} 
+                        alt='directionnal-arrow' 
+                        className='dir-arrow dir-arrow__left' 
+                        style={{ display: hide() }}
+                        onClick={() => indexMinus()}
+
+                        ></img>
+                        <img 
+
+                        src={arrow} 
+                        alt='directionnal-arrow' 
+                        className='dir-arrow dir-arrow__right' 
+                        style={{ display: hide() }}
+                        onClick={() => indexPlus()}
+
+                        ></img>
+                    </div>
+                    <p className='index' style={{ display: hide() }}>
+                        {`${currentIndex + 1}/${pictures.length}`}
+                    </p>
+                </div>
+            
+
+        </div>
     )
 }
 
